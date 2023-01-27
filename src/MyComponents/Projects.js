006e4edf00ec2c764375e1a479d4d6d84284315b
BikeshSuwal <bikeshsuwal776@gmail.com>
2023-01-27 13:32:24 +0545

    empty cards added for future projects

--- src/MyComponents/Projects.js
+++ src/MyComponents/Projects.js
@@ -2,10 +2,43 @@ import React from "react";
 import Calculator from "./Projects/Calculator";
 import Counter from "./Projects/Counter";
 import TextConvert from "./Projects/TextConvert";
+import "./Projects.css";
 
 export default function Projects() {
   return (
     <div>
+      <div class="card-container">
+        <div class="card">
+          <img src="project1.jpg" alt="Project 1" />
+          <div class="card-content">
+            <h2>Project 1</h2>
+            <p>A brief description of Project 1.</p>
+            <a href="#" class="card-link">
+              View Project
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="project2.jpg" alt="Project 2" />
+          <div class="card-content">
+            <h2>Project 2</h2>
+            <p>A brief description of Project 2.</p>
+            <a href="#" class="card-link">
+              View Project
+            </a>
+          </div>
+        </div>
+        <div class="card">
+          <img src="project2.jpg" alt="Project 3" />
+          <div class="card-content">
+            <h2>Project 3</h2>
+            <p>A brief description of Project 3.</p>
+            <a href="#" class="card-link">
+              View Project
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="container">
         <TextConvert />
       </div>
