c695d3fb7cc61231139edd849cd406bb5591c2d5
BikeshSuwal <bikeshsuwal776@gmail.com>
2023-01-28 23:10:07 +0545

    toggle buttton toggles on clicking items in it

--- src/MyComponents/header.js
+++ src/MyComponents/header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ToggleButton from "./ToggleButton";
+// import ToggleButton from "./ToggleButton";
 
 export const Header = () => {
+  const Toggle = () => {
+    console.log("button clicked");
+    const navitems = document.getElementsByClassName("navitems")[0];
+    navitems.classList.toggle("active");
+    // return (
+    //   <>
+    //     <div>button clicked</div>
+    //   </>
+    // );
+  };
   return (
     <>
       <div className="navbar" id="sticky">
@@ -10,23 +20,36 @@ export const Header = () => {
           <img src="logo.png" alt="BIKESH SUWAL" />
           <h2>BIKESH SUWAL</h2>
         </div>
-        <ToggleButton />
+
         <div className="navitems">
           <ul className="mainmenu">
             <li className="home">
-              <Link to="/">HOME</Link>
+              <Link to="/" onClick={Toggle}>
+                HOME
+              </Link>
             </li>
             <li className="projects">
-              <Link to="/projects">PROJECTS</Link>
+              <Link to="/projects" onClick={Toggle}>
+                PROJECTS
+              </Link>
             </li>
             <li className="about">
-              <Link to="/about">ABOUT</Link>
+              <Link to="/about" onClick={Toggle}>
+                ABOUT
+              </Link>
             </li>
             <li className="contacts">
-              <Link to="/contacts">CONTACTS</Link>
+              <Link to="/contacts" onClick={Toggle}>
+                CONTACTS
+              </Link>
             </li>
           </ul>
         </div>
+        <button href="#" className="toggle-button" onClick={Toggle}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
       </div>
     </>
   );
