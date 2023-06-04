90f7ef76298aec3e53e36dfe50231a1b7039d027
unknown <bikeshsuwal776@gmail.com>
2023-06-04 13:19:29 +0545

    Updates

---
+++ precache-manifest.25cbf41e45adba80b946699a9c276469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d01b574c0a7b14d16e0",
+    "url": "/mysite/static/css/main.0c0743fc.chunk.css"
+  },
+  {
+    "revision": "4d01b574c0a7b14d16e0",
+    "url": "/mysite/static/js/main.43236002.chunk.js"
+  },
+  {
+    "revision": "048b85102e2b9026b2d1",
+    "url": "/mysite/static/js/runtime~main.33502b54.js"
+  },
+  {
+    "revision": "a2e5cad04a496deb6423",
+    "url": "/mysite/static/js/2.9d630efa.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/mysite/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "b5d9b36f7b4861056cd0d67d5d6245c4",
+    "url": "/mysite/index.html"
+  }
+];
