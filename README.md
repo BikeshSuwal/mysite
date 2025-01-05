d1fcd17fbb1d1816a217832035770fa90dddb02b
Bikesh Suwal <bikeshsuwal776@gmail.com>
2025-01-05 20:54:19 +0545

    add gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-This is a portfolio website + the playground for me to learn, try and test web development.
-Unfortunately, the hosting site where this site was hosted for free has terminated its service. 
-Will get back again soon.  https://suwalbikesh.com.np/
---
+++ build/precache-manifest.40bc2f5a9363c88f3c18f83daca8a0db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7f62b473762e5a85f7d",
+    "url": "/static/css/main.029727ec.chunk.css"
+  },
+  {
+    "revision": "e7f62b473762e5a85f7d",
+    "url": "/static/js/main.ef714811.chunk.js"
+  },
+  {
+    "revision": "ef596ee87d1781b14ae8",
+    "url": "/static/js/runtime~main.68481440.js"
+  },
+  {
+    "revision": "a2e5cad04a496deb6423",
+    "url": "/static/js/2.9d630efa.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "86edeaa6e9f8c4ea3665ac9c3ca01a5b",
+    "url": "/index.html"
+  }
+];
